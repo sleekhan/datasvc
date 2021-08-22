e66021496065fc928e816e581649fdddf6ec4767
Ryan Han <sleekhan@Ryanui-Macmini.local>
2021-08-22 13:38:59 +0900

    Add Webclient and CircuitBreaker

--- src/main/scala/org/ryan/services/datasvc/DatasvcApplication.scala
+++ src/main/scala/org/ryan/services/datasvc/DatasvcApplication.scala
@@ -1,7 +1,15 @@
 package org.ryan.services.datasvc
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+import io.github.resilience4j.timelimiter.TimeLimiterConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.circuitbreaker.resilience4j.{ReactiveResilience4JCircuitBreakerFactory, Resilience4JConfigBuilder}
+import org.springframework.cloud.client.circuitbreaker.Customizer
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.client.WebClient
+
+import java.time.Duration
 
 @SpringBootApplication
 class DatasvcApplication
@@ -9,6 +17,19 @@ class DatasvcApplication
 object DatasvcApplication {
   def main(args: Array[String]): Unit = {
     SpringApplication.run(classOf[DatasvcApplication], args: _*)
+  }
+
+  @Bean
+  def webClientBuilder(): WebClient.Builder = {
+    WebClient.builder()
+  }
+
+  @Bean
+  def defaultCustomizer(): Customizer[ReactiveResilience4JCircuitBreakerFactory] = {
+    factory =>
+      factory.configureDefault(id => new Resilience4JConfigBuilder(id)
+        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build()).build())
   }
 }
 
